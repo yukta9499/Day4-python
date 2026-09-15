ea6e9d38c015ace23f3a4e82aa9f7fc5e0329c2a
yukta9499 <yuktakale3105@gmail.com>
2026-09-15 09:51:47 +0600

    file

--- while.py
+++ while.py
@@ -1,3 +1,13 @@
-while True :
-    string = input("Enter a String : ")
-    print(string)
+# while True :
+#     string = input("Enter a String : ")
+#     print(string)
+
+correct_pass = "some_pass"
+not_found = True
+
+while not_found:
+    passw = input("Enter pass :")
+    if passw == correct_pass:
+        not_found = False
+        
+print("The password is matching")
